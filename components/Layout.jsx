4fa58827d3aafba1c10a1f19d495bf298015f3cd
wuaicot <wuaicot@gmail.com>
2025-03-10 05:41:41 -0300

    deployed

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Head from "next/head";
-
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const Layout = ({ children }) => {
   return (
@@ -17,6 +17,7 @@ const Layout = ({ children }) => {
       <footer>
         <Footer />
       </footer>
+      <SpeedInsights />
     </div>
   );
 };
