6c4cc1356b8721f7d5f5225883bf8fd9d3b39d68
wuaicot <wuaicot@gmail.com>
2025-04-03 10:24:18 -0300

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Toaster } from "react-hot-toast";
-
+import { Analytics } from "@vercel/analytics/react"
 import { Layout } from "../components";
 import "../styles/globals.css";
 import { StateContext } from "../context/StateContext";
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
       <Layout>
+       <Analytics />
         <Toaster />
         <Component {...pageProps} />
       </Layout>
